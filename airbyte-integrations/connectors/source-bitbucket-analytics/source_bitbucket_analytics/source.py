f075293359f112ba1439275fb40ccf8a85c46270
JoaoJanini <jjaninir@gmail.com>
2022-01-24 10:43:07 -0300

    Configured refresh token

--- airbyte-integrations/connectors/source-bitbucket-analytics/source_bitbucket_analytics/source.py
+++ airbyte-integrations/connectors/source-bitbucket-analytics/source_bitbucket_analytics/source.py
@@ -3,17 +3,12 @@
 #
 
 import re
-from typing import Any, Dict, List, Mapping, Tuple, MutableMapping
-
+from typing import Any, Dict, List, Mapping, Tuple
 from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http.auth import MultipleTokenAuthenticator
-from airbyte_cdk.sources.streams.http.auth import Oauth2Authenticator
-from requests.auth import AuthBase
-import json
-import requests
+from airbyte_cdk.sources.streams.http.requests_native_auth.oauth import Oauth2Authenticator
 
 from .streams import (
     Branches,
@@ -26,79 +21,14 @@ from .streams import (
     RepositoryStats,
 )
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.json file.
-"""
-
 TOKEN_SEPARATOR = ","
 # To scan all the repos within orgnaization, workspace name could be
 # specified by using asteriks i.e. "airbytehq/*"
 workspace_PATTERN = re.compile("^.*/\\*$")
 
-class BitbucketAuthenticator(AuthBase):
-    """
-    Temporary method to get access token
-    """
-
-    def __init__(
-        self,
-        client_id: str,
-        client_secret: str,
-        access_token_name: str = "access_token",
-    ):
-        self.client_secret = client_secret
-        self.client_id = client_id
-        self.access_token_name = access_token_name
-        self.access_token_endpoint = "https://bitbucket.org/site/oauth2/access_token"
-        self._access_token = None
-
-
-    def __call__(self, request):
-        request.headers.update(self.get_auth_header())
-        return request
-
-    def get_auth_header(self) -> Mapping[str, Any]:
-        return {"Authorization": f"Bearer {self.get_access_token()}"}
-
-    def get_access_token(self):
-        return self.access_token()
-
-    def get_request_body(self) -> Mapping[str, Any]:
-        """Override to define additional parameters"""
-        payload: MutableMapping[str, Any] = {
-            "grant_type": "client_credentials",
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-        }
-
-        return payload
-
-    def access_token(self) -> Tuple[str, int]:
-        """
-        returns a tuple of (access_token, token_lifespan_in_seconds)
-        """
-        try:
-            response = requests.request(method="POST", url=self.access_token_endpoint, data=self.get_request_body())
-            response.raise_for_status()
-            access_token = json.loads(response.content)["access_token"]            
-            return access_token
-        except Exception as e:
-            raise Exception(f"Error while getting the access token: {e}") from e
-
-
 class SourceBitbucketAnalytics(AbstractSource):
     @staticmethod
-    def _generate_repositories(config: Mapping[str, Any], authenticator: BitbucketAuthenticator) -> Tuple[List[str], List[str]]:
+    def _generate_repositories(config: Mapping[str, Any], authenticator: Oauth2Authenticator) -> Tuple[List[str], List[str]]:
         """
         Parse repositories config line and produce two lists of repositories.
         Args:
@@ -128,21 +58,12 @@ class SourceBitbucketAnalytics(AbstractSource):
 
     @staticmethod
     def _get_authenticator(config: Dict[str, Any]):
-        # Before we supported oauth, personal_access_token was called `access_token` and it lived at the
-        # config root. So we first check to make sure any backwards compatbility is handled.
-        token = config.get("access_token")
-        if not token:
-            creds = config.get("credentials")
-            token = creds.get("access_token") or creds.get("personal_access_token")
-        tokens = [t.strip() for t in token.split(TOKEN_SEPARATOR)]
 
         client_id = config.get("client_id")
         client_secret = config.get("client_secret")
+        refresh_token = config.get("refresh_token")
 
-
-        # return Oauth2Authenticator(client_id=client_id, client_secret=client_secret)
-        # return MultipleTokenAuthenticator(tokens=tokens, auth_method="Bearer")
-        return BitbucketAuthenticator(client_id=client_id, client_secret=client_secret)
+        return Oauth2Authenticator(token_refresh_endpoint="https://bitbucket.org/site/oauth2/access_token", client_id=client_id, client_secret=client_secret, refresh_token=refresh_token)
 
 
     @staticmethod
@@ -188,8 +109,7 @@ class SourceBitbucketAnalytics(AbstractSource):
             # In case of getting repository list for given workspace was
             # successfull no need of checking stats for every repository within
             # that workspace.
-            # Since we have "repo" scope requested it should grant access to private repos as well:
-            # https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps#available-scopes
+            
             repositories, _ = self._generate_repositories(config=config, authenticator=authenticator)
 
             repository_stats_stream = RepositoryStats(
